1aaf34e087dfdd15337204778db1ea6f6ca5b731
hankyul1103 <hankyul1103@naver.com>
2020-09-18 13:22:51 +0900

    hankyul1103

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500,500);
+  fill(255, 0, 0);
+  ellipse(100, 100, 50, 50);
+  fill(0, 255, 0);
+  ellipse( 170,170, 85, 85);
+  fill(0, 0, 255)
+  ellipse(50, 50, 25, 25);
+  fill(0, 255, 255);
+  ellipse(200, 200, 100, 100);
+  fill(203, 21, 180);
+  ellipse(130, 130, 65, 65);
+}
+
+
+function draw() {
+
+}

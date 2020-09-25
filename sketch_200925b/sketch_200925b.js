40b8137bae461fae664b9990d21af475c53f6beb
hankyul1103 <hankyul1103@naver.com>
2020-09-25 14:35:20 +0900

    이거 아까 했던 거니까 무시해주세요

--- sketch_200925b/sketch_200925b.js
+++ sketch_200925b/sketch_200925b.js
@@ -1,5 +1,14 @@
 function setup() {
-   ///oma1103;
+  createCanvas(500, 500);
+  
+  for(let i=0; i<500; i+=100){
+    rect(i, 0, 100, 100);
+    rect(i, 100, 100, 100);
+    rect(i, 200, 100, 100);
+    rect(i, 300, 100, 100);
+    rect(i, 400, 100, 100);
+    
+  }
 }
 
 
